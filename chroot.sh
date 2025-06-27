ea6fd52b734e807e90ae502fbb6be4976aceb294
Wang Xuancong <xuancong84@gmail.com>
2025-06-27 14:19:46 +0800

    updated to Ubuntu 24.04.2 LTS

--- chroot.sh
+++ chroot.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+safe_umount() {
+	if ! umount "$@"; then
+		umount -fl "$@"
+	fi
+}
+
+
+cd "`dirname $0`"
+
 for p in sys proc dev tmp; do
 	if ! mountpoint nfs/ro/rootfs/$p; then
 		mount -v --bind /$p nfs/ro/rootfs/$p
 	fi
 done
+mount -v --bind nfs/rw/var nfs/ro/rootfs/var
 
-chroot nfs bash -l
+chroot nfs/ro/rootfs bash -l
 
+umount -f nfs/ro/rootfs/var
 for p in sys proc dev tmp; do
-	umount -f nfs/$p
+	umount -f nfs/ro/rootfs/$p
 done
 
